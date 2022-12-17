623a31acdeac416a71bd841af999a721aeb4e782
MPandey9 <madhusudanpandey9582@gmail.com>
2022-12-17 16:31:50 +0530

    states added

--- assets/js/main.js
+++ assets/js/main.js
@@ -202,6 +202,42 @@
   });
 
   /**
+   * Products Slider
+   */
+  new Swiper('.products-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 20
+      },
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 20
+      }
+    }
+  });
+
+  /**
    * Porfolio isotope and filter
    */
   window.addEventListener('load', () => {
